f86bce364dc3e9bb4a26286d1ddebcc4e85d8b8c
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-04-26 18:29:28 -0400

    changed color from purple to blue

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for(let y = 1; y <= 10; y++) {
     for(let x = 1; x <= 10; x++) {
 
         if(y == 1 || x == 1) {
-            table = "purple";
+            table = "#6886b7";
         }
         else {
             table = "grey";
